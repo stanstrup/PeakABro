846a355e4e36d731cc4c832fa5dc4e1ebfe5dab0
Jan Stanstrup <stanstrup@gmail.com>
2017-10-19 14:52:08 +0200

    Added all functions to extract data from databases (the ones added so far)

--- R/generate_cmp_tbl.R
+++ R/generate_cmp_tbl.R
@@ -1,3 +1,125 @@
+# Helper functions --------------------------------------------------------
+
+#' Robust helper functions to process structures with rcdk
+#' 
+#' Some helper functions modified from rcdk to return NA on failure.
+#' This makes it easier to process many compounds where some might behave unexpected.
+#' 
+#' @importFrom rcdk do.aromaticity do.typing do.isotopes get.exact.mass
+#' @importFrom purrr possibly
+#' @importFrom rinchi parse.inchi
+#' @return Returns functions equavalent of \code{\link{do.aromaticity}}, \code{\link{do.typing}}, \code{\link{do.isotopes}}, \code{\link{get.exact.mass}} but made robust with \code{\link{possibly}}.
+#' 
+#' @keywords internal
+#' 
+#' @describeIn do.aromaticity_p Robust version of \code{\link{do.aromaticity}}
+do.aromaticity_p <- possibly(do.aromaticity, NA)
+
+#' @describeIn do.aromaticity_p Robust version of \code{\link{do.typing}} 
+do.typing_p <-  possibly(do.typing, NA)
+
+#' @describeIn do.aromaticity_p Robust version of \code{\link{do.isotopes}}
+do.isotopes_p <-  possibly(do.isotopes, NA)
+
+#' @describeIn do.aromaticity_p Robust version of \code{\link{get.exact.mass}}
+get.exact.mass_p <- possibly(get.exact.mass, NA)
+
+#' @describeIn do.aromaticity_p Robust version of \code{\link{parse.inchi}}
+parse.inchi_p <- possibly(parse.inchi, NA)
+
+
+
+#' Helper functions to extract info from LipidBlast json.
+#'
+#' Some helper functions to extract info from LipidBlast json.
+#'
+#' @param json LipidBlast json
+#'
+#' @importFrom dplyr %>%
+#' @importFrom rlist list.select list.flatten
+#' @return Returns character vector with the relevant info.
+#'
+#' @keywords internal
+#'
+#' @describeIn LB_getname Extracts the compound name from LipidBlast json
+LB_getname <- function(json){
+    # make check happy
+     . <- name <- NULL
+     
+    json %>% 
+        {.$compound[[1]]$names} %>% 
+        list.select(name) %>% 
+        list.flatten %>% 
+        paste(collapse="; ")
+    }
+
+#' @describeIn LB_getname Extracts the inchi from LipidBlast json
+LB_get_inchi <- function(json){
+    # make check happy
+    . <- NULL
+     
+    json %>% 
+        {.$compound[[1]]$inchi}
+}
+
+#' @describeIn LB_getname Extracts the id name from LipidBlast json
+LB_get_id <- function(json){
+    # make check happy
+    . <- NULL
+     
+    json %>% 
+    {.$id}
+}
+
+
+
+#' Inchi to formula
+#'
+#' @param inchi Character vector with inchis
+#'
+#' @return Character vector
+# '
+#' @export
+#'
+#' @importFrom stringr str_split_fixed
+#'
+inchi2formula <- function(inchi){
+    str_split_fixed(inchi,"/",3)[,2]
+}
+
+
+
+#' Inchi to mass
+#'
+#' @param inchi Character vector with inchis
+#'
+#' @return Numeric vector
+# '
+#' @export
+#'
+#'
+
+inchi2mass <- function(inchi){
+
+    mol <- parse.inchi_p(inchi)[[1]]
+
+    do.aromaticity_p(mol)
+    do.typing_p(mol)
+    do.isotopes_p(mol)
+    mass <- get.exact.mass_p(mol)
+
+    return(mass)
+}
+
+
+
+# formula2mass  <- . %>% {map_chr(., ~getMass(getMolecule(..1)))} #  rdisop is crashy on windows 10
+
+
+
+# Functions to extract a table of data from different database for --------
+
+
 #' Generate table with LipidMaps compounds
 #'
 #' @param sdf_path Path to the Lipidmaps SDF file available at: http://www.lipidmaps.org/resources/downloads/
@@ -13,37 +135,89 @@
 #' @examples
 #' \dontrun{
 #' lipidmaps_tbl <- generate_lipidmaps_tbl("path/to/LMSDFDownload6Dec16FinalAll.sdf")
+#' 
+#' # Alternatively get pre-made table from the package
+#' lipidmaps_tbl2 <- readRDS(system.file("extdata", "lipidmaps_tbl.rds", package="PeakABro"))
 #' }
 #'
-#' 
+#'
 
 generate_lipidmaps_tbl <- function(sdf_path){
-    
+
     # make check happy
-    COMMON_NAME <- 
-    SYNONYMS <- 
-    SYSTEMATIC_NAME <- 
-    INCHI <- 
-    FORMULA <- 
-    EXACT_MASS <- 
-    LM_ID <- 
-    name <- 
-    mass <- 
+    COMMON_NAME <-
+    SYNONYMS <-
+    SYSTEMATIC_NAME <-
+    INCHI <-
+    FORMULA <-
+    EXACT_MASS <-
+    LM_ID <-
+    name <-
+    mass <-
         NULL
-    
+
     SDF <- read.SDFset(sdf_path)
-    
+
     SDF_table <- datablock2ma(datablock(SDF))
 
-    lipidmaps_tbl <- SDF_table %>% 
-                     as_data_frame %>% 
+    lipidmaps_tbl <- SDF_table %>%
+                     as_data_frame %>%
                      replace_na(list(COMMON_NAME = "", SYNONYMS = "", SYSTEMATIC_NAME = "")) %>%
                      unite(name,COMMON_NAME, SYNONYMS, SYSTEMATIC_NAME, sep="; ") %>%
                      mutate(name = sub("^; .{1}", "", name)) %>%
                      mutate(name = sub("; $", "", name)) %>%
                      mutate(name = gsub("; ; ", "; ", name)) %>%
-                     select(id = LM_ID, name, inchi = INCHI, formula = FORMULA, mass = EXACT_MASS) %>% 
+                     select(id = LM_ID, name, inchi = INCHI, formula = FORMULA, mass = EXACT_MASS) %>%
                      mutate(mass=as.numeric(mass))
-    
+
     return(lipidmaps_tbl)
 }
+
+
+
+#' Generate table with LipidBlast compounds
+#'
+#' @param json_path Path to the LipidBlast json file available at: http://mona.fiehnlab.ucdavis.edu/downloads
+#'
+#' @return tbl A \code{\link{tibble}} containing the columns: id, name, inchi, formula, and mass.
+# '
+#' @export
+#'
+#' @importFrom jsonlite read_json
+#' @importFrom tibble data_frame
+#' @importFrom purrr map_chr map_dbl
+#' @importFrom dplyr %>% mutate
+#' @importFrom magrittr %<>%
+#' @examples
+#' \dontrun{
+#' lipidblast_tbl <- generate_lipidblast_tbl("path/to/MoNA-export-LipidBlast.json")
+#' 
+#' # Alternatively get pre-made table from the package
+#' lipidblast_tbl2 <- readRDS(system.file("extdata", "lipidblast_tbl.rds", package="PeakABro"))
+#' }
+#'
+#'
+
+generate_lipidblast_tbl <- function(json_path){
+
+    # make check happy
+    . <-
+        inchi <-
+        NULL
+
+    lipidblast <- read_json(json_path)
+
+    lipidblast_tbl <- lipidblast %>% {data_frame(
+                                                  id = map_chr(.,LB_get_id),
+                                                  name = map_chr(.,LB_getname),
+                                                  inchi = map_chr(.,LB_get_inchi)
+                                                )
+                                     }
+
+
+    lipidblast_tbl %<>% mutate(formula = inchi2formula(inchi)) %>%
+                        # mutate(mass = formula2mass(formula)) %>%
+                        mutate(mass = map_dbl(inchi, inchi2mass)) # slow but rdisop is crashy on windows 10
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# PeakABro
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+PeakABro
+========
+
 Peaklist Annotator and Browser
+
+The purpose of this package is to help identification in metabolomics by: 1) extracting tables of compounds with their relevant info from different compound database formats 2) annotating an XCMS (or other) peaklist with the compounds from the compound databases 3) displaying an interactive browseable peaktable with the annotated compounds in nested tables
+
+Get compound tables from databases
+----------------------------------
+
+Several compound databases can be downloaded and from and their database format we can extract a table of relevant information. So far I have added functions to extract in from the downloadable LipidMaps and LipidBlast.
+
+-   Download the LipidMaps SDF from: <http://www.lipidmaps.org/resources/downloads/>
+-   Download the LipidBlast json from: <http://mona.fiehnlab.ucdavis.edu/downloads>
+-   Adjust path to where you have downloaded the files.
+
+``` r
+library(PeakABro)
+lipidmaps_tbl <- generate_lipidmaps_tbl("inst/extdata/LMSDFDownload6Dec16FinalAll.sdf")
+## Warning in read.SDFset(sdf_path): 424 invalid SDFs detected. To fix, run:
+## valid <- validSDF(sdfset); sdfset <- sdfset[valid]
+lipidblast_tbl <- generate_lipidblast_tbl("inst/extdata//MoNA-export-LipidBlast.json")
+```
+
+``` r
+lipidmaps_tbl
+## # A tibble: 40,772 x 5
+##              id
+##           <chr>
+##  1 LMFA00000001
+##  2 LMFA00000002
+##  3 LMFA00000003
+##  4 LMFA00000004
+##  5 LMFA00000005
+##  6 LMFA00000006
+##  7 LMFA00000007
+##  8 LMFA00000008
+##  9 LMFA00000009
+## 10 LMFA00000014
+## # ... with 40,762 more rows, and 4 more variables: name <chr>,
+## #   inchi <chr>, formula <chr>, mass <dbl>
+```
+
+``` r
+lipidblast_tbl
+## # A tibble: 135,456 x 5
+##                  id                             name
+##               <chr>                            <chr>
+##  1 LipidBlast000001      CerP 24:0; CerP(d14:0/10:0)
+##  2 LipidBlast000002      CerP 26:0; CerP(d14:0/12:0)
+##  3 LipidBlast000003      CerP 28:0; CerP(d14:0/14:0)
+##  4 LipidBlast000004      CerP 30:0; CerP(d14:0/16:0)
+##  5 LipidBlast000005      CerP 32:0; CerP(d14:0/18:0)
+##  6 LipidBlast000006  CerP 32:1; CerP(d14:0/18:1(9Z))
+##  7 LipidBlast000007      CerP 34:0; CerP(d14:0/20:0)
+##  8 LipidBlast000008      CerP 36:0; CerP(d14:0/22:0)
+##  9 LipidBlast000009      CerP 38:0; CerP(d14:0/24:0)
+## 10 LipidBlast000010 CerP 38:1; CerP(d14:0/24:1(15Z))
+## # ... with 135,446 more rows, and 3 more variables: inchi <chr>,
+## #   formula <chr>, mass <dbl>
+```
+
+This takes rather long because the databases are quite large. Therefore I have configured
+
+Alternatively get pre-made table from the package
+=================================================
+
+``` r
+lipidmaps_tbl2 <-  readRDS(system.file("extdata", "lipidmaps_tbl.rds",  package="PeakABro"))
+lipidblast_tbl2 <- readRDS(system.file("extdata", "lipidblast_tbl.rds", package="PeakABro"))
+```
